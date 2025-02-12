ea8118c6dbc782ba42090be832c01036432caa2c
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-12 17:58:03 -0300

    Add files via upload

---
+++ vm1/variable.tf
@@ -0,0 +1,35 @@
+variable "rg_name" {
+    description = "Nome Resource Group (Marcelo)"
+    type = string
+}
+
+variable "rg_location" {
+    description = "Localização RG"
+    type = string
+}
+
+variable "vnet_name" {
+    description = "Nome vnet"
+    type = string
+}
+
+variable "snet_name" {
+    description = "Nome snet"
+    type = string
+}
+
+
+variable "nic_name" {
+    description = "Nome nic1"
+    type = string
+}
+
+variable "ip_vm1" {
+    description = "Nome ip vm1"
+    type = string
+}
+
+variable "vm1_name" {
+    description = "Nome ip vm1"
+    type = string
+}
